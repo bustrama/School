eb1d5010d0593a2e66a656dbb0845019114a49e8
unknown <user@user-5ca4556f15.lab.local>
2014-12-11 15:39:28 +0200

    added new project, school

---
+++ OOP/Inheritance/school/school/Worker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace school
+{
+    class Worker:Person
+    {
+        protected float salary { get; set; }
+        protected float hours { get; set; }
+
+        public Worker(string first, string last, int age, string id, string num, string department, float salary, float hours):
+            base(first, last, age, id)
+        {
+            this.salary = salary;
+            this.hours = hours;
+        }
+
+        public void SetWorker(string first, string last, int age, string id, string num, string dpt, float salary, float hours)
+        {
+            base.SetPerson(first, last, age, id);
+            this.salary = salary;
+            this.hours = hours;
+        }
+
+        public void SetWorkerSalary(float salary, float hour)
+        {
+            this.salary = salary;
+            this.hours = hour;
+        }
+        
+        public float CalcSalary()
+        {
+            return this.salary * this.hours;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n Salary: " + this.salary + "\n Hour: " + this.hours;
+        }
+    }
+}
